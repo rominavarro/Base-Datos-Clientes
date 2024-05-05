effecb4a7a6b6df0fe91b5974ab6f2f68fb19349
rominavarro <rosio.navarro31@gmail.com>
2024-05-05 00:03:38 -0400

    Button Function Update

--- client/gui_db.py
+++ client/gui_db.py
@@ -47,6 +47,8 @@ class mainframe(tk.Frame):
         #self.config(background="#00A99D")
 
         self.InfoCliente()
+        self.InputDeActivate()
+
 
     def InfoCliente(self):
         #labels para el nombre de cliente
@@ -86,57 +88,94 @@ class mainframe(tk.Frame):
 
         #Inputs de cada label
         #input para el nombre
-        self.entry_nombre = tk.Entry(self)
-        self.entry_nombre.config(width=50, state = 'disable', font=('Consolas', 12, 'bold'))
-        self.entry_nombre.grid(column=1, row=0, padx=10, pady=10)
+        self.nombre_clear = tk.StringVar()
+        self.entry_nombre = tk.Entry(self, textvariable=self.nombre_clear)
+        self.entry_nombre.config(width=50, font=('Consolas', 12, 'bold'))
+        self.entry_nombre.grid(column=1, row=0, padx=10, pady=10, columnspan=2)
 
         #input para el numero del cliente
-        self.entry_num_client = tk.Entry(self)
-        self.entry_num_client.config(width=50, state = 'disable', font=('Consolas', 12, 'bold'))
-        self.entry_num_client.grid(column=1, row=1, padx=10, pady=10)
+        self.num_client_clear = tk.StringVar()
+        self.entry_num_client = tk.Entry(self, textvariable=self.num_client_clear)
+        self.entry_num_client.config(width=50, font=('Consolas', 12, 'bold'))
+        self.entry_num_client.grid(column=1, row=1, padx=10, pady=10, columnspan=2)
         
         #input para el telefono
-        self.entry_telefono = tk.Entry(self)
-        self.entry_telefono.config(width=50, state = 'disable', font=('Consolas', 12, 'bold'))
-        self.entry_telefono.grid(column=1, row=2, padx=10, pady=10)
+        self.telefono_clear = tk.StringVar()
+        self.entry_telefono = tk.Entry(self, textvariable=self.telefono_clear)
+        self.entry_telefono.config(width=50, font=('Consolas', 12, 'bold'))
+        self.entry_telefono.grid(column=1, row=2, padx=10, pady=10, columnspan=2)
         
         #input para el numero de orden
-        self.entry_num_orden = tk.Entry(self)
-        self.entry_num_orden.config(width=50, state = 'disable', font=('Consolas', 12, 'bold'))
-        self.entry_num_orden.grid(column=1, row=3, padx=10, pady=10)
+        self.num_orden_clear = tk.StringVar()
+        self.entry_num_orden = tk.Entry(self, textvariable=self.num_orden_clear)
+        self.entry_num_orden.config(width=50, font=('Consolas', 12, 'bold'))
+        self.entry_num_orden.grid(column=1, row=3, padx=10, pady=10, columnspan=2)
 
         #input para articulos
-        self.entry_articulos = tk.Entry(self)
-        self.entry_articulos.config(width=50, state = 'disable', font=('Consolas', 12, 'bold'))
-        self.entry_articulos.grid(column=1, row=3, padx=10, pady=10)
+        self.articulos_clear = tk.StringVar()
+        self.entry_articulos = tk.Entry(self, textvariable=self.articulos_clear)
+        self.entry_articulos.config(width=50, font=('Consolas', 12, 'bold'))
+        self.entry_articulos.grid(column=1, row=4, padx=10, pady=10, columnspan=2)
 
         #input para fecha
-        self.entry_fecha = tk.Entry(self)
-        self.entry_fecha.config(width=50, state = 'disable', font=('Consolas', 12, 'bold'))
-        self.entry_fecha.grid(column=1, row=5, padx=10, pady=10)
+        self.fecha_clear = tk.StringVar()
+        self.entry_fecha = tk.Entry(self, textvariable=self.fecha_clear)
+        self.entry_fecha.config(width=50, font=('Consolas', 12, 'bold'))
+        self.entry_fecha.grid(column=1, row=5, padx=10, pady=10, columnspan=2)
         
         #input para comercial
-        self.entry_comercial = tk.Entry(self)
-        self.entry_comercial.config(width=50, state = 'disable', font=('Consolas', 12, 'bold'))
-        self.entry_comercial.grid(column=1, row=6, padx=10, pady=10)
+        self.comercial_clear = tk.StringVar()
+        self.entry_comercial = tk.Entry(self, textvariable=self.comercial_clear)
+        self.entry_comercial.config(width=50, font=('Consolas', 12, 'bold'))
+        self.entry_comercial.grid(column=1, row=6, padx=10, pady=10, columnspan=2)
 
         #Botones
         
         #Boton Agregar
-        self.button_add = tk.Button(self, text="Agregar")
-        self.button_add.config(width=20, font=('Consolas', 12, 'bold'), background="#F0563D", foreground="black")
+        self.button_add = tk.Button(self, text="Agregar", command=self.InputActivate)
+        self.button_add.config(width=20, font=('Consolas', 12, 'bold'), background="#F0563D", foreground="black", cursor='hand2', activebackground="#C13A2B")
         self.button_add.grid(row=7, column=0, padx=10, pady=10)
 
         #Boton Modificar
-        self.button_modify = tk.Button(self, text="Modificar")
-        self.button_modify.config(width=20, font=('Consolas', 12, 'bold'), background="#00A99D", foreground="black")
+        self.button_modify = tk.Button(self, text="Modificar", command=self.InputActivate)
+        self.button_modify.config(width=20, font=('Consolas', 12, 'bold'), background="#00A99D", foreground="black", cursor='hand2', activebackground="#07685F")
         self.button_modify.grid(row=7, column=1, padx=10, pady=10)
 
         #Boton Eliminar
-        self.button_erase = tk.Button(self, text="Eliminar")
-        self.button_erase.config(width=20, font=('Consolas', 12, 'bold'), background="#000000", foreground="white")
+        self.button_erase = tk.Button(self, text="Eliminar", command=self.InputDeActivate)
+        self.button_erase.config(width=20, font=('Consolas', 12, 'bold'), background="#000000", foreground="white", cursor='hand2', activebackground="#494949")
         self.button_erase.grid(row=7, column=2, padx=10, pady=10)
 
+    def InputActivate(self):
+        self.entry_nombre.config(state='normal')
+        self.entry_num_client.config(state='normal')
+        self.entry_telefono.config(state='normal')
+        self.entry_fecha.config(state='normal')
+        self.entry_num_orden.config(state='normal')
+        self.entry_articulos.config(state='normal')
+        self.entry_comercial.config(state='normal')
 
+        #self.button_add.config(state='normal')
+        self.button_modify.config(state='normal')
+        self.button_erase.config(state='normal')
+        
+    def InputDeActivate(self):
+        self.nombre_clear.set('')
+        self.num_client_clear.set('')
+        self.num_orden_clear.set('')
+        self.fecha_clear.set('')
+        self.telefono_clear.set('')
+        self.comercial_clear.set('')
+        self.articulos_clear.set('')
 
-      
+        self.entry_nombre.config(state='disable')
+        self.entry_num_client.config(state='disable')
+        self.entry_telefono.config(state='disable')
+        self.entry_fecha.config(state='disable')
+        self.entry_num_orden.config(state='disable')
+        self.entry_articulos.config(state='disable')
+        self.entry_comercial.config(state='disable')
+
+        #self.button_add.config(state='disable')
+        self.button_modify.config(state='disable')
+        self.button_erase.config(state='disable')
